c671447b76128985c34bf89ec1da0dbff37db03d
Shariful <shariful0411@gmail.com>
2025-05-10 16:20:22 +0600

    about us title added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const AboutPage = () => {
     return (
-        <div>
-            THis is about Page 
+        <div className='wrapper px-4 sm:px-6 '>
+            <h1 className='text-3xl font-semibold text-center my-8'>Come Explore Our School Family</h1> 
+            <p className='font-medium leading-[29px] text-lg'>Welcome to <strong>Little Stars School</strong>, a joyful place where young minds begin their journey of discovery, learning, and growth. At our school, we believe every child is unique and full of potential. Our mission is to create a safe, caring, and fun environment where children feel loved, valued, and inspired to explore the world around them. With passionate teachers, playful activities, and a strong foundation in early education, we help kids grow with confidence, creativity, and curiosity. Here, every day is a new adventure — filled with laughter, learning, and lots of love!
+
+</p>
         </div>
     );
 };
